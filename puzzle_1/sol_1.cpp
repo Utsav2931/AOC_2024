a7cfec67ef6857c0d65f800637fbd429ff6ee09f
Utsav <upatel231a@gmail.com>
2024-12-01 12:03:09 -0800

    Solved 1st day puzzle

---
+++ puzzle_1/sol_1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> getValues (string line) {
+	string num1, num2;
+	int i = 0;
+	while (line[i] != ' ') {
+		num1 += line[i];
+	  i += 1;
+	}
+
+	while (line[i] == ' ')
+	{
+		i += 1;
+	}
+	
+	while (i < line.size()) {
+		num2 += line[i];
+		i += 1;
+	}
+
+	return {stoi(num1), stoi(num2)};
+
+}
+
+int main() {
+	string line;
+	ifstream InputFile("input.txt");
+	vector<int> list1, list2;
+	int ans = 0;
+
+	while (getline (InputFile, line)) {
+		cout << line<<endl;
+		pair<int, int> nums = getValues(line);
+		// cout<<nums.first<<" "<<nums.second<<endl;
+		list1.push_back(nums.first);
+		list2.push_back(nums.second);
+	}
+
+	sort(list1.begin(), list1.end());
+	sort(list2.begin(), list2.end());
+
+	for (int i = 0; i < list1.size(); i++) {
+		ans += abs(list1[i] - list2[i]);
+	}
+
+	cout<<ans<<endl;
+
+	return 0;
+}
---
+++ puzzle_1/sol_2.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> getValues (string line) {
+	string num1, num2;
+	int i = 0;
+	while (line[i] != ' ') {
+		num1 += line[i];
+	  i += 1;
+	}
+
+	while (line[i] == ' ')
+	{
+		i += 1;
+	}
+	
+	while (i < line.size()) {
+		num2 += line[i];
+		i += 1;
+	}
+
+	return {stoi(num1), stoi(num2)};
+
+}
+
+int main() {
+	string line;
+	ifstream InputFile("input.txt");
+	vector<int> list1, list2;
+  unordered_map <int, int> count;
+	int ans = 0;
+
+	while (getline (InputFile, line)) {
+		cout << line<<endl;
+		pair<int, int> nums = getValues(line);
+		// cout<<nums.first<<" "<<nums.second<<endl;
+		list1.push_back(nums.first);
+		list2.push_back(nums.second);
+    count[nums.second] += 1;
+	}
+  
+  for (auto x : list1) {
+    if (count.find(x) != count.end()) 
+      ans += (x * count[x]);
+  }
+
+  cout<<ans<<endl;
+
+	return 0;
+}
